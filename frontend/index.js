c2de1aa99710ed22747045435b87122ff9fd04f0
Ashish <iamashking123@gmail.com>
2022-12-06 18:56:01 -0800

    connected contract with basic website

--- frontend/index.js
+++ frontend/index.js
@@ -1,5 +1,6 @@
 import { ethers } from "./ethers-5.6.esm.min.js";
-console.log(ethers);
+
+import { abi, contractAddress } from "./constants.js";
 
 const connectButton = document.getElementById("connectButton");
 
@@ -8,14 +9,25 @@ connectButton.onclick = connect;
 async function connect() {
     if (typeof window.ethereum !== "undefined") {
         try {
-            await ethereum.request({ method: "eth_requestAccounts" });
+            const a = await ethereum.request({ method: "eth_requestAccounts" });
+            connectButton.innerHTML = "Connected";
         } catch (error) {
             console.log(error);
         }
-        connectButton.innerHTML = "Connected";
-        const accounts = await ethereum.request({ method: "eth_accounts" });
-        console.log(accounts);
     } else {
         connectButton.innerHTML = "Please install MetaMask";
     }
+    getCandidates();
+}
+
+async function getCandidates() {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    console.log("adsf");
+    const signer = provider.getSigner();
+    console.log("sdf");
+    const contract = new ethers.Contract(contractAddress, abi, signer);
+    console.log(contract);
+
+    var transactionResponse = await contract.getNumOfVoters();
+    console.log(transactionResponse);
 }
