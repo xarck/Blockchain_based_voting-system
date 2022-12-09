0763b37ce412fa13c25b20fe5b74c17f7fbedca5
Ashish <iamashking123@gmail.com>
2022-12-08 18:28:06 -0800

    deployed to alchemy

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,10 +1,19 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
+
+const { API_URL, PRIVATE_KEY } = process.env;
+
 module.exports = {
     solidity: "0.8.17",
+    defaultNetwork: "goerli",
     networks: {
         hardhat: {},
+        goerli: {
+            url: API_URL,
+            accounts: [`0x${PRIVATE_KEY}`],
+        },
         localhost: {
             url: "http://127.0.0.1:8545/",
             chainId: 31337,
--- backend/scripts/deploy.js
+++ backend/scripts/deploy.js
@@ -1,23 +1,32 @@
 // imports
 const { ethers, run, network } = require("hardhat");
-
+const fs = require("fs");
 // async main
 async function main() {
-    const ElectionFactory = await ethers.getContractFactory("Election");
-    console.log("Deploying contract...");
-    const electionStorage = await ElectionFactory.deploy();
-    await electionStorage.deployed();
+    // let provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+    // let wallet = new ethers.wallet(process.env.PRIVATE_KEY, provider);
 
-    console.log(`Deployed contract to: ${electionStorage.address}`);
-    // what happens when we deploy to our hardhat network?
-    if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
-        console.log("Waiting for block confirmations...");
-        await electionStorage.deployTransaction.wait(6);
-        await verify(electionStorage.address, []);
-    }
+    // const abi = fs.readFileSync("./");
+    // const ElectionFactory = await ethers.getContractFactory("Election");
+    // console.log("Deploying contract...");
+    // const electionStorage = await ElectionFactory.deploy();
+    // await electionStorage.deployed();
 
-    const getNumOfCandidates = await electionStorage.getNumOfCandidates();
-    console.log(`Current Value is: ${getNumOfCandidates}`);
+    // console.log(`Deployed contract to: ${electionStorage.address}`);
+    // // what happens when we deploy to our hardhat network?
+    // if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
+    //     console.log("Waiting for block confirmations...");
+    //     await electionStorage.deployTransaction.wait(6);
+    //     await verify(electionStorage.address, []);
+    // }
+
+    // const getNumOfCandidates = await electionStorage.getNumOfCandidates();
+    // console.log(`Current Value is: ${getNumOfCandidates}`);
+    const Election = await ethers.getContractFactory("Election");
+
+    // Start deployment, returning a promise that resolves to a contract object
+    const ELECION_CONTRACT = await Election.deploy();
+    console.log("Contract deployed to address:", ELECION_CONTRACT.address);
 }
 
 // async function verify(contractAddress, args) {
